05f7e07875c170f06b747b1a59324ec5d8b2a650
Bianca Salino <biancadsalino@gmail.com>
2022-07-22 00:11:14 -0300

    First component

--- frontend/react-ts/src/App.tsx
+++ frontend/react-ts/src/App.tsx
@@ -1,6 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
+
 function App() {
-  return(
-    <h1>Olá Mundo</h1>
+  return (
+    <> {/*Para exportar mais de uma tag */}
+      <h1>Olá Mundo</h1>
+      <NotificationButton />
+    </>
   )
 }
 
