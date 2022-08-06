b147881da0b4cc74d331224eb56a318c93539c2d
Bianca Salino <biancadsalino@gmail.com>
2022-08-05 22:58:47 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -1,10 +1,11 @@
 package com.devsuperior.dsmeta.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -18,9 +19,13 @@ public class SaleController {
 	@Autowired //injeta a instancia 
 	private SaleService service;
 	
-	@GetMapping // uma das  operações que tem na hora de fazer requisição; obter dados do backend
-	public List<Sale> findSales() {
-		return service.findSales();
+	@GetMapping // uma das  operações que tem na hora de fazer requisição, obter dados do backend
+	//controllador recebe os dados como texto e depois converte pra dados ,a na web os dados trafegam como texto, por isso string
+	public Page<Sale> findSales(
+			@RequestParam(value = "minDate", defaultValue = "") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue = "")String maxDate, 
+			Pageable pageable) {
+		return service.findSales(minDate, maxDate, pageable);
 	}
 	
 }
--- backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/service/SaleService.java
@@ -1,8 +1,12 @@
 package com.devsuperior.dsmeta.service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -16,8 +20,17 @@ public class SaleService {
 	private SaleRepository repository;
 	
 	@GetMapping
-	public List<Sale> findSales() {
-		return repository.findAll();
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+		
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		//converte os textos para o tipo localDate
+		LocalDate min = minDate.equals("")? today.minusDays(365): LocalDate.parse(minDate); // traz um ano atras
+		LocalDate max = minDate.equals("")? today: LocalDate.parse(maxDate); // traz a data atual
+		
+		
+		
+		return repository.findSales(min, max, pageable);
 		
 	}
 
